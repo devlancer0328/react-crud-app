3af8dada0acef7e56985a4b49c382063d9b36dcd
devlancer0328 <topdev0328@proton.me>
2023-01-26 21:55:26 +0100

    add Edit component to Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Home from './components/Home';
+import Edit from './components/Edit';
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/edit" element={<Edit />} />
         </Routes>
     </div>
   )
