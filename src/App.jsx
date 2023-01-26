26868aa2bec5c3b522ad73675aa26fd6bdd23833
devlancer0328 <topdev0328@proton.me>
2023-01-26 21:13:42 +0100

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from './components/Home';
 
-function App() {
+const App = () => {
 
   return (
     <div className="App">
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
     </div>
   )
 }
