83baa94ecb267972a9e18fe6377ebbd723a86831
devlancer0328 <topdev0328@proton.me>
2023-01-26 22:06:20 +0100

    add more data

--- src/components/Guests.js
+++ src/components/Guests.js
@@ -23,6 +23,31 @@ const Guests = [
         id: "5",
         name: "Ewa Chicken",
         age: "30"
+    },
+    {
+        id: "6",
+        name: "Daniel Johnson",
+        age: "50"
+    },
+    {
+        id: "7",
+        name: "Martin Caren",
+        age: "28"
+    },
+    {
+        id: "8",
+        name: "Adam Zaremba",
+        age: "34"
+    },
+    {
+        id: "9",
+        name: "Aneta Sikora",
+        age: "40"
+    },
+    {
+        id: "10",
+        name: "Daniel Piwowarski",
+        age: "33"
     }
 ]
 
