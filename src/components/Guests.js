86797c0a542944c73be998bf996ade8d74e74ba8
devlancer0328 <topdev0328@proton.me>
2023-01-26 21:13:56 +0100

    add guests data

---
+++ src/components/Guests.js
@@ -0,0 +1,29 @@
+const Guests = [
+    {
+        id: "1",
+        name: "Daniel Craig",
+        age: "24"
+    },
+    {
+        id: "2",
+        name: "Martin Lambert",
+        age: "18"
+    },
+    {
+        id: "3",
+        name: "Adam Newski",
+        age: "30"
+    },
+    {
+        id: "4",
+        name: "Adam Kitchorski",
+        age: "23"
+    },
+    {
+        id: "5",
+        name: "Ewa Chicken",
+        age: "30"
+    }
+]
+
+export default Guests;
