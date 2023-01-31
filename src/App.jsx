4ef726f8513efa16bd6d6490bb0e8f01cd05b992
devlancer0328 <topdev0328@proton.me>
2023-01-31 15:50:16 +0100

    add ```add``` route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Home from './components/Home';
 import Edit from './components/Edit';
+import Add from './components/Add';
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/edit" element={<Edit />} />
+          <Route path="/add" element={<Add />} />
         </Routes>
     </div>
   )
