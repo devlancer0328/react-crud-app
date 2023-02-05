cb01067bfd432af5dc05ee62424dba74b58fdffe
devlancer0328 <topdev0328@proton.me>
2023-02-05 17:53:08 +0100

    handle adding data

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import Guests from "./Guests";
 
 const Add = () => {
 
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+
+    let history = useNavigate();
+
     const handleData = (e) => {
         e.preventDefault();
+
+        let randomValue = Math.floor(Math.random()*100);
+        
+        Guests.push({id: randomValue, name: name, age: age});
+
+        history('/');
     };
 
     return ( 
@@ -16,19 +29,14 @@ const Add = () => {
                 <Form className="d-grip">
                     <div className="mb-1 mt-2">
                         <label className="mr-3" htmlFor="name">Name</label>
-                        <input required type="text" name="name" id="name" placeholder="Enter name"/>
+                        <input required type="text" name="name" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.target.value)}/>
                     </div>
                     <div className="mb-1 mt-2">
                         <label className="ml-3 mr-3" htmlFor="age">Age</label>
-                        <input required type="number" name="age" id="age" placeholder="Enter age"/>
+                        <input required type="number" name="age" id="age" placeholder="Enter age" value={age} onChange={e => setAge(e.target.value)}/>
                     </div>
-                    <input className="btn btn-info mt-4" type="submit" value="Add a new data" />
+                    <Button onClick={(e) => handleData(e)} className="btn btn-info mt-4">Submit</Button>
                 </Form>
-            </div>
-            <div>
-                <Link to='/'>
-                    <Button onClick={() => handleData()}>Back to Guest Book</Button>
-                </Link>
             </div>
         </>
      );
